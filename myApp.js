11db355337fc3fe8f53efe83a179dc29167ce679
Amir Hosein Nazari <czhablog@gmail.com>
2022-08-24 12:59:44 +0400

    added the contentSecurityPolicy middleware

--- myApp.js
+++ myApp.js
@@ -11,9 +11,10 @@ ninetyDaysInSeconds = 90 * 24 * 60 * 60
 app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
 app.use(helmet.dnsPrefetchControl())
 app.use(helmet.noCache())
-
-
-
+app.use(helmet.contentSecurityPolicy({directives: {
+  defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", "trusted-cdn.com"]
+  }}))
 
 
 
