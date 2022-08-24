5037c05c96172510a1499997f79a93513ae73871
Amir Hosein Nazari <czhablog@gmail.com>
2022-08-24 12:46:23 +0400

    added the dnsPrefetchControl

--- myApp.js
+++ myApp.js
@@ -8,7 +8,8 @@ app.use(helmet.xssFilter())
 app.use(helmet.noSniff())
 app.use(helmet.ieNoOpen())
 ninetyDaysInSeconds = 90 * 24 * 60 * 60
-app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}))
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
+app.use(helmet.dnsPrefetchControl())
 
 
 
